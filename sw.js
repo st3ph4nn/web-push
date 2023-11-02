0df559b7b9122ac626f92e781338d25ec351feb1
Stephan Nguyen <stephan.nguyen@alliander.com>
2023-11-02 16:44:19 +0100

    updated push.js

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
-self.addEventListener('push', () => {
-    self.registration.sendNotification('Test Subscribe Message', {});
-})
+self.addEventListener("push", (event) => {
+  const options = {
+    body: "This notification was generated from a push!",
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: "2",
+    },
+    actions: [
+      { action: "explore", title: "Explore this new world" },
+      { action: "close", title: "Close" },
+    ],
+  };
+
+  event.waitUntil(
+    self.registration.showNotification("Test Subscribe Message", options)
+  );
+});

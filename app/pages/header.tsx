b44197b35f6f5264b7ae8ceba59c8c222c99bcb1
Stephan Nguyen <snguyen@pancompany.com>
2023-11-02 20:17:07 +0100

    added netlify file

--- app/pages/header.tsx
+++ app/pages/header.tsx
@@ -1,18 +1,16 @@
-'use client';
+"use client";
 
 export default function Header() {
-
-    function getAPI(){
-        fetch("/api/hello").then((res) => {
-            console.log(res)
-        })
-    }
-
+  function getAPI() {
+    fetch("/api/hello").then((res) => res.json()).then((finalRes) => {
+      console.log(finalRes);
+    });
+  }
 
   return (
     <main>
-        <h1>NextJs Test</h1>
-        <button onClick={getAPI}>Subscribe</button>
+      <h1>NextJs Test</h1>
+      <button onClick={getAPI}>Subscribe</button>
     </main>
-  )
+  );
 }
